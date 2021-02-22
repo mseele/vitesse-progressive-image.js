d9e579d5a76327f706417831d172d9009ccd5578
mseele <mseele@gmail.com>
2021-02-22 19:44:37 +0100

    add progressive-image.js demo

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import 'windi.css'
-import './styles/main.css'
-import { ViteSSG } from 'vite-ssg'
-import generatedRoutes from 'pages-generated'
-import { setupLayouts } from 'layouts-generated'
-import App from './App.vue'
+import "windi.css";
+import "./styles/main.css";
+import { ViteSSG } from "vite-ssg";
+import generatedRoutes from "pages-generated";
+import { setupLayouts } from "layouts-generated";
+import App from "./App.vue";
+import "progressive-image.js/dist/progressive-image.js";
+import "progressive-image.js/dist/progressive-image.css";
 
-const routes = setupLayouts(generatedRoutes)
+const routes = setupLayouts(generatedRoutes);
 
 // https://github.com/antfu/vite-ssg
-export const createApp = ViteSSG(
-  App,
-  { routes },
-  (ctx) => {
-    // install all modules under `modules/`
-    Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
-  },
-)
+export const createApp = ViteSSG(App, { routes }, (ctx) => {
+  // install all modules under `modules/`
+  Object.values(import.meta.globEager("./modules/*.ts")).map((i) =>
+    i.install?.(ctx)
+  );
+});
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { useI18n } from 'vue-i18n'
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useI18n } from "vue-i18n";
 
-const name = ref('')
+const name = ref("");
 
-const router = useRouter()
+const router = useRouter();
 const go = () => {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
+  if (name.value) router.push(`/hi/${encodeURIComponent(name.value)}`);
+};
 
-const { t } = useI18n()
+const { t } = useI18n();
 </script>
 
 <template>
@@ -20,13 +19,30 @@ const { t } = useI18n()
       <carbon-campsite class="inline-block" />
     </p>
     <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
+      <a
+        rel="noreferrer"
+        href="https://github.com/antfu/vitesse"
+        target="_blank"
+      >
         Vitesse
       </a>
     </p>
     <p>
-      <em class="text-sm opacity-75">{{ t('intro.desc') }}</em>
+      <em class="text-sm opacity-75">{{ t("intro.desc") }}</em>
     </p>
+
+    <div class="p-4 w-80 h-50 mx-auto overflow-hidden">
+      <a
+        href="https://images.unsplash.com/photo-1510279770292-4b34de9f5c23?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
+        class="progressive replace"
+      >
+        <img
+          src="https://images.unsplash.com/photo-1510279770292-4b34de9f5c23?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=20&q=80"
+          class="preview"
+          alt="image"
+        />
+      </a>
+    </div>
 
     <div class="py-4" />
 
@@ -40,16 +56,12 @@ const { t } = useI18n()
       class="px-4 py-2 text-sm text-center bg-transparent border border-gray-200 rounded outline-none active:outline-none dark:border-gray-700"
       style="width: 250px"
       @keydown.enter="go"
-    >
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
+    />
+    <label class="hidden" for="input">{{ t("intro.whats-your-name") }}</label>
 
     <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
+      <button class="m-3 text-sm btn" :disabled="!name" @click="go">
+        {{ t("button.go") }}
       </button>
     </div>
   </div>
